e887b79e0db72a7c8d132e2d4fe5fd303e005f55
Daarkito <90762237+Daarkito@users.noreply.github.com>
2021-11-23 17:15:17 +0100

    Add files via upload

---
+++ piramide numerica.py
@@ -0,0 +1,11 @@
+def piramide_numerica():
+    fila_completa=""
+    num=input ("Dame un numero wachin: ")
+    for fil in range (num,0,-1):
+        for col in range (1,fil+1):
+            fila_completa=fila_completa+str(fil)
+        print (fila_completa)
+        fila_completa=""
+       
+       
+piramide_numerica()

4bf5684d10beac8d6ff4732fd47a7a23fe756d81
Daarkito <90762237+Daarkito@users.noreply.github.com>
2021-12-14 15:24:36 +0100

    Add files via upload

---
+++ inicio.cpp
@@ -0,0 +1,11 @@
+//Esto es un comentario
+/*Esto es un comentario de
+más líneas*/
+//INSTRUCCIONES AL PREPROCESADOR
+#include<stdio.h>//FICHERO DE CABECERA
+int main(){
+    printf("Hola mundo, cruel");
+    return 0;
+   
+   
+}
